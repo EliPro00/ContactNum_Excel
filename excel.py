5acf67af543894e7098cdcbdad60e31b8b02250f
Elyas Sayed <elyassayed05@gmail.com>
2025-03-08 13:02:43 +0400

    deleeted

--- excel.py
+++
@@ -1,7 +0,0 @@
-from openpyxl import Workbook , load_workbook 
-
-wb = load_workbook ('test.xlsx')
-ws = wb.active
-new_row = {'A' : 'sas_cust' , 'b' : '+971666666'}
-ws.append(new_row)
-wb.save('test.xlsx')
